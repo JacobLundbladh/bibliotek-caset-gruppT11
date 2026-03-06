205ec09ee391d1d2ff210ba416c7bd7e7835c9ad
Jacob <jacob2005@hotmail.se>
2026-03-06 14:27:18 +0100

    Skapa MVC projekt

---
+++ bibliotekssystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace bibliotekssystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
