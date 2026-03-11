b157f67a53a4d0f820f27dea267dbc3fc29e0260
Jacob <jacob2005@hotmail.se>
2026-03-11 20:00:53 +0100

    Skapa api för lån

---
+++ bibliotekssystem/Services/LoanService.cs
@@ -0,0 +1,28 @@
+﻿using bibliotekssystem.Models;
+
+namespace bibliotekssystem.Services;
+
+public class LoanService
+{
+    private HttpClient _httpClient;
+
+    public LoanService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<Loan[]> GetLoan()
+    {
+        try // Fel hantering
+        {
+
+            var result = await _httpClient.GetFromJsonAsync<Loan[]>("loan");
+            return result ??  Array.Empty<Loan>(); // Ifall null skicka till backa tom array
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return Array.Empty<Loan>();
+        }
+    }
+}
---
+++ loanService/Migrations/20260309175633_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace loanService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Loans",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ItemID = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false),
+                    LoanDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loans", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Loans");
+        }
+    }
+}
