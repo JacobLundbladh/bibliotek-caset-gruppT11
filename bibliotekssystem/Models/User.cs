8d9eb7ee01dcf1cb09c7195da0ffdd3d5a2eb989
Jacob <jacob2005@hotmail.se>
2026-03-23 11:36:08 +0100

    Lade till User model

---
+++ bibliotekssystem/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace bibliotekssystem.Models;
+
+public class User
+{
+    public int Id { get; set; } // PK key
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    public required string Role { get; set; }
+
+}
