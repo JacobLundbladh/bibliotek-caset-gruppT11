2c2d6a5df651d736b02a4254fbf5cf07a9e6b782
Jacob <jacob2005@hotmail.se>
2026-03-23 19:48:43 +0100

    Lade till funktion för att hämta alla konton och ta bort konton

--- bibliotekssystem/Services/AccountService.cs
+++ bibliotekssystem/Services/AccountService.cs
@@ -33,4 +33,36 @@ public class AccountService
     }
     
     
+    // Hämta alla Konton
+  
+    public async Task<Account[]> GetAccounts()
+    {
+        try // Fel hantering
+        {
+
+            var result = await _httpClient.GetFromJsonAsync<Account[]>("User");
+            return result ??  Array.Empty<Account>(); // Ifall null skicka till backa tom array
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return Array.Empty<Account>();
+        }
+    }
+
+    public async Task<bool> DeleteAccounts(int id)
+    {
+        try // Fel hantering
+        {
+
+            var result = await _httpClient.DeleteAsync($"User/{id}");
+            return true; // Ifall null skicka till backa tom array
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return false;
+        }
+    }
+    
 }
