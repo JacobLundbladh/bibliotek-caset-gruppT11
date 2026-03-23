c946df2489b4d9225d18229bb22e05836b900544
Dreni <drenipeci@gm>
2026-03-23 10:58:23 +0100

    added Items  MVC (views and controllers)

---
+++ bibliotek-case-gruppT11/LibraryMVC/Controllers/ItemsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using LibraryMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryMVC.Controllers;
+
+public class ItemsController : Controller
+{
+    private readonly HttpClient _http = new HttpClient();
+
+    public async Task<IActionResult> Index()
+    {
+        var items = await _http.GetFromJsonAsync<List<Item>>("https://localhost:XXXX/api/items");
+        return View(items ?? new List<Item>());
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Item item)
+    {
+        await _http.PostAsJsonAsync("https://localhost:XXXX/api/items", item);
+        return RedirectToAction("Index");
+    }
+}
---
+++ bibliotek-case-gruppT11/LibraryMVC/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryMVC.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public bool IsAvailable { get; set; }
+}
