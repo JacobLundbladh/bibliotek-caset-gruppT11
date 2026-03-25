12b617291890f174b4dba0a01f04b300f6661a20
Laraeb Alam <alamlaraeb@gmail.com>
2026-03-25 17:28:03 +0100

    swagger i reminder

--- Library.ReminderAPI/Program.cs
+++ Library.ReminderAPI/Program.cs
@@ -12,11 +12,8 @@ builder.Services.AddDbContext<LibraryContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
