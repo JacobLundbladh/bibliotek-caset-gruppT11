71c8150965d85f07507dd3b3acb68440508d9da9
Dreni <drenipeci@gm>
2026-03-25 14:39:02 +0100

    Added EF Core SQLite database with full CRUD functionality

---
+++ Library.ItemAPI/Data/LibraryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Library.ItemAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.ItemAPI.Data;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Item> Items { get; set; }
+}
