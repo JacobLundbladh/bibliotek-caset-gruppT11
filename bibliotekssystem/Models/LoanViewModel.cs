487097968b25fd1d36232d04c6fd51b44d262fc8
Jacob <jacob2005@hotmail.se>
2026-03-26 13:37:15 +0100

    Skapade en ny model för att visa både loan och item data

---
+++ bibliotekssystem/Models/LoanViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace bibliotekssystem.Models;
+
+public class LoanViewModel
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string ItemTitle { get; set; } = "";
+    public DateTime LoanDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+}
