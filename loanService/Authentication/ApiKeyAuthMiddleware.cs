4c5edb32360ebaddd48dd7ce6298b75e27f9ac30
Jacob <jacob2005@hotmail.se>
2026-03-28 11:24:19 +0100

    Skapade sidor för api nyckel logik

---
+++ loanService/Authentication/ApiKeyAuthMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace loanService.Authentication;
+
+public class ApiKeyAuthMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _configuration;
+    
+    public  ApiKeyAuthMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        _next = next;
+        _configuration = configuration;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(AuthConstanst.ApiKeyHeaderName, out var extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Apinyckel saknas");
+            return;
+        }
+        var apiKey = _configuration["Authentication:ApiKey"];
+        if (!apiKey.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Apinyckel felaktig");
+            return;
+        }
+        await _next(context);
+    }
+}
