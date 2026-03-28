0639b3702a3e6c46a1a9715af2466cb808eaa669
Dreni <drenipeci@gm>
2026-03-28 10:17:27 +0100

    la till api nyckeln även i bibliotekssystemet s

--- bibliotekssystem/Controllers/ItemController.cs
+++ bibliotekssystem/Controllers/ItemController.cs
@@ -8,10 +8,24 @@ public class ItemController : Controller
 {
     private readonly HttpClient _http = new HttpClient();
 
+    // URL till ditt ItemAPI i Azure
     private readonly string baseUrl = "https://app-sos100-itemapi-dreni-h7hfczh6g8fxb8gv.norwayeast-01.azurewebsites.net";
+
+    // API-nyckeln hämtas från konfiguration (Azure Environment Variable)
+    private readonly string _apiKey;
+
+    public ItemController(IConfiguration config)
+    {
+        // Hämtar nyckeln som vi la in i Azure (ItemApiKey)
+        _apiKey = config["ItemApiKey"]!;
+
+        // Lägger till API-nyckeln i headern för alla anrop
+        _http.DefaultRequestHeaders.Add("X-API-Key", _apiKey);
+    }
 
     public async Task<IActionResult> Index()
     {
+        // Hämtar alla items från ItemAPI
         var items = await _http.GetFromJsonAsync<List<Item>>($"{baseUrl}/api/Items");
         return View(items ?? new List<Item>());
     }
@@ -25,6 +39,7 @@ public class ItemController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(Item item)
     {
+        // Skickar nytt item till API (med API-nyckel automatiskt i headern)
         await _http.PostAsJsonAsync($"{baseUrl}/api/Items", item);
         return RedirectToAction("Index");
     }
